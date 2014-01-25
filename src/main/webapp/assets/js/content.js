bebf88019643d8b7ced5af39bc2d080c45c1c3f0
chenxiaojing123 <chxj1992@gmail.com>
2014-01-25 11:26:55 +0800

    save

--- src/main/webapp/assets/js/content.js
+++ src/main/webapp/assets/js/content.js
@@ -1,25 +1,10 @@
 //content
-define('content',['jquery','bootstrap','user'],function(require, exports, module) {
+define('content',['jquery','bootstrap','user','utils/datetime'],function(require, exports, module) {
 
 
     var $ = require('jquery');
     var user = require('user');
-
-    Date.prototype.format = function (fmt) {
-        var o = {
-            "M+": this.getMonth() + 1,
-            "d+": this.getDate(),
-            "H+": this.getHours(),
-            "m+": this.getMinutes(),
-            "s+": this.getSeconds(),
-            "q+": Math.floor((this.getMonth() + 3) / 3),
-            "S": this.getMilliseconds()
-        };
-        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-        for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        return fmt;
-    }
+    var datetime = require('datetime');
 
     $(document).ready(function(){
 
@@ -116,7 +101,7 @@ define('content',['jquery','bootstrap','user'],function(require, exports, module
 
                 if (data.status == 1){
                     $("#content-"+contentId+"-context > span").text(content);
-                    $("#content-"+contentId+"-context > small").text(new Date().format('yyyy-MM-dd HH:mm:ss'));
+                    $("#content-"+contentId+"-context > small").text(datetime.format('yyyy-MM-dd HH:mm:ss'));
                 } else {
                     $("#content-alert").fadeIn();
                     $("#content-error-info").text(data.info);
