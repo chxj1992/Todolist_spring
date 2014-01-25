c836a4761d338eecb5873d9e47b036e7ea5d9b40
chenxiaojing123 <chxj1992@gmail.com>
2014-01-25 10:41:58 +0800

    change something

--- web/assets/js/sea_modules/bootstrap.js
+++
@@ -1,3 +0,0 @@
-define('bootstrap',['../bower_components/jquery/jquery.min.js','../bower_components/bootstrap/dist/js/bootstrap.min.js'],function() {
-    // 这里并没有使 bootstrap 成功依赖 jquery，原因不明 ～
-});
--- web/assets/js/sea_modules/jquery.js
+++
@@ -1,3 +0,0 @@
-define('jquery',['../bower_components/jquery/jquery.min.js'],function() {
-    return jQuery.noConflict();
-});
